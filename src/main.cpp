8bb9ecf60eb10406ba3f4bc7ea5799f0fa151d7c
H3m326 <hmunguiaestrada@gmail.com>
2026-06-22 18:10:22 -0500

    feat(test): blink led test for enviorment configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED 19
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32-C3!");
+
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    
+  digitalWrite(LED, !digitalRead(LED));
+  delay(500); // this speeds up the simulation
 }
