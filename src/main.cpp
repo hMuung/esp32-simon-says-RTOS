3957141e510dd41955aa0b497e53a47ff1599dee
hMuung <hmunguiaestrada@gmail.com>
2026-06-23 14:21:33 -0500

    feat(tone): buzzer tone implemented for each button and isr improved

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include <pitches.h>
+
 // Hardware pin mappings
 const uint8_t buttonPins[] = {1, 3, 10, 2};
 const uint8_t ledPins[] = {18, 8, 19, 9};
+#define SPEAKER_PIN 0
 
 // Volatile state variables for ISR-to-main communication
 volatile int lastButton = -1;
@@ -13,7 +16,10 @@ volatile uint32_t lastInterruptTime[] = {0, 0, 0, 0};
 // FreeRTOS timer array for leds
 TimerHandle_t ledTimers[4];
 
-// Software debounce interval in milliseconds
+// Tone button array
+const int gameTones[] = { NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4};
+
+// Software intervals in milliseconds
 const int bounceThreshold = 150;
 const int ledFadeTime = 150;
 
@@ -48,6 +54,9 @@ void setup() {
     );
   }
 
+  // Initialize the tone generator
+  tone(SPEAKER_PIN, 440, 1); 
+
   // Attach falling-edge hardware interrupts to each wrapper
   attachInterrupt(digitalPinToInterrupt(buttonPins[0]), button0ISR, FALLING);
   attachInterrupt(digitalPinToInterrupt(buttonPins[1]), button1ISR, FALLING);
@@ -63,14 +72,23 @@ void IRAM_ATTR handleButtonInterrupt(uint8_t buttonIndex) {
 
   // Ignore interrupts triggered within the debounce threshold
   if ((currentTime - lastInterruptTime[buttonIndex]) >= bounceThreshold) {
+
     lastInterruptTime[buttonIndex] = currentTime;
     lastButton = buttonIndex;
     digitalWrite(ledPins[buttonIndex],HIGH);
-    xTimerStart( ledTimers[buttonIndex], 0);
+    tone(SPEAKER_PIN,gameTones[buttonIndex]);
+
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xTimerStartFromISR(ledTimers[buttonIndex], &xHigherPriorityTaskWoken);
+    if (xHigherPriorityTaskWoken) {
+        portYIELD_FROM_ISR();
+    }
+    
   }
 }
 
 void ledOffCallback(TimerHandle_t xTimer) {
   uint32_t button = (uint32_t) pvTimerGetTimerID(xTimer);
   digitalWrite(ledPins[button],LOW);
+  noTone(SPEAKER_PIN);
 }
